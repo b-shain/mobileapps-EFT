b6e2743476711025cb21ab4d53fd69325b9f0539
Be <Be@24.238.107.128>
2014-11-05 19:32:21 -0500

    added second activity - home activity. starting design

--- src/edu/temple/encryptedfiletransfer/HomeActivity.java
+++ src/edu/temple/encryptedfiletransfer/HomeActivity.java
@@ -5,12 +5,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-//this will be the login activity. 
-public class MainActivity extends Activity {
+//this is where the user selects their friend to send a file or accepts file send requests 
+public class HomeActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_home);
 	}
 }
